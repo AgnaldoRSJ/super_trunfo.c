21e336cad7a947c6e9779da383197cec73d74ff2
Agnaldo Rodrigues da Silva Junior <juninho.vilharba@gmail.com>
2025-09-07 20:02:17 +0000

    preimeira parte do desafio

---
+++ super_trunfo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+   int main(){
+
+    int turisticos;
+    char carta, estado, codigo, cidade [50];
+    float populacao, area, pib;
+
+    printf("Carta:01 \n");
+
+    printf("Estado:");
+    scanf("%s", &estado);
+
+    printf("Código:");
+    scanf("%s", &codigo);
+
+    printf("Nome da Cidade:");
+    scanf("%s", &cidade);
+
+    printf("População:");
+    scanf("%f", &populacao);
+
+    printf("Área:");
+    scanf("%f", &area);
+    
+    printf("PIB:");
+    scanf("%f", &pib);
+
+    printf("Números de pontos Turisticos:");
+    scanf("%d", &turisticos);
+
+       
+
+
+
+   }
